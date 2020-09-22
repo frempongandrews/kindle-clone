0039b98b063722e8ad0902b97991a9d0b71998b7
Andrews Frempong <andrews.frempong@wowcher.co.uk>
2020-09-22 03:42:51 +0100

    Initial Commit

---
+++ kindle-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kindle-app
+//
+//  Created by Andrews Frempong on 22/09/2020.
+//  Copyright © 2020 Andrews Frempong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
